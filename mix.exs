56c69773ed273d4157ed71ce7c2f8b3ad62de68f
Marc Neudert <marc.neudert@gmail.com>
2017-10-30 12:38:00 +0100

    release v0.1.0

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule MMDB2Decoder.Mixfile do
     [
       app: :mmdb2_decoder,
       name: "MMDB2 Decoder",
-      version: "0.1.0-dev",
+      version: "0.1.0",
       elixir: "~> 1.3",
       deps: deps(),
       elixirc_paths: elixirc_paths(Mix.env()),
@@ -39,7 +39,7 @@ defmodule MMDB2Decoder.Mixfile do
     [
       extras: ["CHANGELOG.md", "README.md"],
       main: "readme",
-      source_ref: "master",
+      source_ref: "v0.1.0",
       source_url: @url_github
     ]
   end
