47f37badd1e7fb3743fb2c17bd464c473f9fd9fc
Marc Neudert <marc.neudert@gmail.com>
2018-05-21 15:19:56 +0200

    improves benchmark script

--- bench/lookup.exs
+++ bench/lookup.exs
@@ -1,22 +1,38 @@
-{meta, tree, data} =
-  [__DIR__, "../data/GeoLite2-City.mmdb"]
-  |> Path.join()
-  |> Path.expand()
-  |> File.read!()
-  |> MMDB2Decoder.parse_database()
+defmodule MMDB2Decoder.Benchmark do
+  def run() do
+    database =
+      [__DIR__, "../data/GeoLite2-City.mmdb"]
+      |> Path.join()
+      |> Path.expand()
 
-Benchee.run(
-  %{
-    "lookup" => fn ->
-      {
-        :rand.uniform(255),
-        :rand.uniform(255),
-        :rand.uniform(255),
-        :rand.uniform(255)
-      }
-      |> MMDB2Decoder.lookup(meta, tree, data)
+    case File.exists?(database) do
+      true -> run_benchmark(database)
+      false -> IO.warn("Expected database not found at #{database}")
     end
-  },
-  warmup: 2,
-  time: 30
-)
+  end
+
+  defp run_benchmark(database) do
+    {meta, tree, data} =
+      database
+      |> File.read!()
+      |> MMDB2Decoder.parse_database()
+
+    Benchee.run(
+      %{
+        "random ip lookup" => fn ->
+          {
+            :rand.uniform(255),
+            :rand.uniform(255),
+            :rand.uniform(255),
+            :rand.uniform(255)
+          }
+          |> MMDB2Decoder.lookup(meta, tree, data)
+        end
+      },
+      warmup: 2,
+      time: 30
+    )
+  end
+end
+
+MMDB2Decoder.Benchmark.run()
