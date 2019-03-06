b3f3b2cacd6176984361a0477323232c2bc9dd7b
Marc Neudert <marc.neudert@gmail.com>
2019-03-06 20:08:27 +0100

    uses fixture download from :geolix_testdata

--- test/helpers/fixture_download.ex
+++ test/helpers/fixture_download.ex
@@ -1,37 +1,19 @@
 defmodule MMDB2Decoder.TestHelpers.FixtureDownload do
   @moduledoc false
 
+  alias Geolix.TestData.MMDB2Fixture
   alias MMDB2Decoder.TestHelpers.FixtureList
 
-  @doc """
-  Returns the full local pathname of a fixture filename.
-  """
-  def local(filename) do
-    [__DIR__, "../fixtures/", filename]
-    |> Path.join()
-    |> Path.expand()
-  end
-
-  @doc """
-  Downloads all fixture files.
-  """
+  def local(filename), do: Path.join([path(), filename])
+  def path, do: Path.join([__DIR__, "../fixtures"])
   def run, do: Enum.each(FixtureList.get(), &download/1)
 
-  defp download({_name, filename, remote}) do
+  defp download({_name, filename}) do
     local = local(filename)
 
     if not File.regular?(local) do
       Mix.shell().info([:yellow, "Downloading fixture database: #{filename}"])
-
-      download_fixture(remote, local)
+      MMDB2Fixture.download(filename, path())
     end
-  end
-
-  defp download_fixture(remote, local) do
-    {:ok, _} = Application.ensure_all_started(:hackney)
-    {:ok, _, _, client} = :hackney.get(remote)
-    {:ok, content} = :hackney.body(client)
-
-    File.write!(local, content)
   end
 end
--- test/helpers/fixture_list.ex
+++ test/helpers/fixture_list.ex
@@ -1,32 +1,16 @@
 defmodule MMDB2Decoder.TestHelpers.FixtureList do
   @moduledoc false
 
-  @baseurl "https://raw.githubusercontent.com/maxmind/MaxMind-DB/master/test-data"
   @fixtures [
-    {:fixture_decoder, "MaxMind-DB-test-decoder.mmdb",
-     "#{@baseurl}/MaxMind-DB-test-decoder.mmdb"},
-    {:fixture_ipv4_24, "MaxMind-DB-test-ipv4-24.mmdb",
-     "#{@baseurl}/MaxMind-DB-test-ipv4-24.mmdb"},
-    {:fixture_ipv4_28, "MaxMind-DB-test-ipv4-28.mmdb",
-     "#{@baseurl}/MaxMind-DB-test-ipv4-28.mmdb"},
-    {:fixture_ipv4_32, "MaxMind-DB-test-ipv4-32.mmdb",
-     "#{@baseurl}/MaxMind-DB-test-ipv4-32.mmdb"},
-    {:fixture_ipv6_24, "MaxMind-DB-test-ipv6-24.mmdb",
-     "#{@baseurl}/MaxMind-DB-test-ipv6-24.mmdb"},
-    {:fixture_ipv6_28, "MaxMind-DB-test-ipv6-28.mmdb",
-     "#{@baseurl}/MaxMind-DB-test-ipv6-28.mmdb"},
-    {:fixture_ipv6_32, "MaxMind-DB-test-ipv6-32.mmdb",
-     "#{@baseurl}/MaxMind-DB-test-ipv6-32.mmdb"},
-    {
-      :fixture_broken_pointers,
-      "MaxMind-DB-test-broken-pointers-24.mmdb",
-      "#{@baseurl}/MaxMind-DB-test-broken-pointers-24.mmdb"
-    },
-    {
-      :fixture_no_ipv4_search_tree,
-      "MaxMind-DB-no-ipv4-search-tree.mmdb",
-      "#{@baseurl}/MaxMind-DB-no-ipv4-search-tree.mmdb"
-    }
+    {:fixture_decoder, "MaxMind-DB-test-decoder.mmdb"},
+    {:fixture_ipv4_24, "MaxMind-DB-test-ipv4-24.mmdb"},
+    {:fixture_ipv4_28, "MaxMind-DB-test-ipv4-28.mmdb"},
+    {:fixture_ipv4_32, "MaxMind-DB-test-ipv4-32.mmdb"},
+    {:fixture_ipv6_24, "MaxMind-DB-test-ipv6-24.mmdb"},
+    {:fixture_ipv6_28, "MaxMind-DB-test-ipv6-28.mmdb"},
+    {:fixture_ipv6_32, "MaxMind-DB-test-ipv6-32.mmdb"},
+    {:fixture_broken_pointers, "MaxMind-DB-test-broken-pointers-24.mmdb"},
+    {:fixture_no_ipv4_search_tree, "MaxMind-DB-no-ipv4-search-tree.mmdb"}
   ]
 
   @doc """
@@ -36,14 +20,13 @@ defmodule MMDB2Decoder.TestHelpers.FixtureList do
 
       {
         :name_as_atom,
-        "local_filename.mmdb",
-        "remote_url"
+        "local_filename.mmdb"
       }
   """
   @spec get() :: list
   def get, do: @fixtures
 
-  Enum.each(@fixtures, fn {name, file, _} ->
+  Enum.each(@fixtures, fn {name, file} ->
     def fixture_name(unquote(name)), do: unquote(file)
   end)
 end
