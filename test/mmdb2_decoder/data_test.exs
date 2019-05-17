4ea37efb7fe88fc7c0b3514c0cf23f6dff947930
Marc Neudert <marc.neudert@gmail.com>
2019-05-17 18:16:48 +0200

    cs

--- test/mmdb2_decoder/data_test.exs
+++ test/mmdb2_decoder/data_test.exs
@@ -128,18 +128,18 @@ defmodule MMDB2Decoder.DataTest do
       |> MMDB2Decoder.parse_database()
       |> MMDB2Decoder.pipe_lookup({1, 1, 1, 0})
 
-    assert decoded[:utf8_string] == "unicode! ☯ - ♫"
-    assert decoded[:double] == 42.123456
+    assert decoded[:array] == [1, 2, 3]
+    assert decoded[:boolean] == true
     assert decoded[:bytes] == <<0, 0, 0, 42>>
+    assert decoded[:double] == 42.123456
+    assert decoded[:float] == 1.1
+    assert decoded[:int32] == -1 * :math.pow(2, 28)
+    assert decoded[:map] == %{mapX: %{arrayX: [7, 8, 9], utf8_stringX: "hello"}}
     assert decoded[:uint16] == 100
     assert decoded[:uint32] == :math.pow(2, 28)
-    assert decoded[:int32] == -1 * :math.pow(2, 28)
     assert decoded[:uint64] == 1 <<< 60
     assert decoded[:uint128] == 1 <<< 120
-    assert decoded[:array] == [1, 2, 3]
-    assert decoded[:map] == %{mapX: %{arrayX: [7, 8, 9], utf8_stringX: "hello"}}
-    assert decoded[:boolean] == true
-    assert decoded[:float] == 1.1
+    assert decoded[:utf8_string] == "unicode! ☯ - ♫"
   end
 
   test "decoded MAX values" do
