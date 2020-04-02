56bd0f877ff16f32ca4526d9b6b331d3a88a4ab8
Marc Neudert <marc.neudert@gmail.com>
2020-04-02 17:59:01 +0200

    Raise development version to 3.0.0-dev

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule MMDB2Decoder.MixProject do
     [
       app: :mmdb2_decoder,
       name: "MMDB2 Decoder",
-      version: "2.2.0-dev",
+      version: "3.0.0-dev",
       elixir: "~> 1.7",
       aliases: aliases(),
       deps: deps(),
