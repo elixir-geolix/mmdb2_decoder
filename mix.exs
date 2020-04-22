ef220ab8a45ac71f4b3ac3b5defe6488fbb2544d
Marc Neudert <marc.neudert@gmail.com>
2020-04-22 18:39:43 +0200

    Define more strict :excoveralls requirement

--- mix.exs
+++ mix.exs
@@ -42,7 +42,7 @@ defmodule MMDB2Decoder.MixProject do
       {:credo, "~> 1.0", only: :dev, runtime: false},
       {:dialyxir, "~> 1.0", only: :dev, runtime: false},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
-      {:excoveralls, "~> 0.12", only: :test, runtime: false},
+      {:excoveralls, "~> 0.12.0", only: :test, runtime: false},
       {:geolix_testdata, "~> 0.5.0", only: [:bench, :test], runtime: false}
     ]
   end
