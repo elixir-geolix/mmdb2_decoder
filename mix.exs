4bb2bb4ad105d5b099698e0793455f39d3c4a62e
Marc Neudert <marc.neudert@gmail.com>
2020-05-17 14:42:42 +0200

    Bump version to v3.1.0-dev

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule MMDB2Decoder.MixProject do
     [
       app: :mmdb2_decoder,
       name: "MMDB2 Decoder",
-      version: "3.0.0",
+      version: "3.1.0-dev",
       elixir: "~> 1.7",
       aliases: aliases(),
       deps: deps(),
@@ -61,7 +61,7 @@ defmodule MMDB2Decoder.MixProject do
   defp docs do
     [
       main: "MMDB2Decoder",
-      source_ref: "v3.0.0",
+      source_ref: "master",
       source_url: @url_github
     ]
   end
