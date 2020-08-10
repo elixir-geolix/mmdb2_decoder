8ec08221fc7fda03a4b742b33bc2a8910907417f
Marc Neudert <marc.neudert@gmail.com>
2020-08-10 17:57:29 +0200

    Bump :geolix_testdata

--- mix.exs
+++ mix.exs
@@ -43,7 +43,7 @@ defmodule MMDB2Decoder.MixProject do
       {:dialyxir, "~> 1.0", only: :dev, runtime: false},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
       {:excoveralls, "~> 0.13.0", only: :test, runtime: false},
-      {:geolix_testdata, "~> 0.5.0", only: [:bench, :test], runtime: false}
+      {:geolix_testdata, "~> 0.5.1", only: [:bench, :test], runtime: false}
     ]
   end
 
