51b08a9b8e5a0fa8824c3cca06b2376c2c5d8cb7
Marc Neudert <marc.neudert@gmail.com>
2021-04-04 10:50:51 +0200

    Update dependencies (and their minimum versions)

--- mix.exs
+++ mix.exs
@@ -41,7 +41,7 @@ defmodule MMDB2Decoder.MixProject do
       {:credo, "~> 1.0", only: :dev, runtime: false},
       {:dialyxir, "~> 1.0", only: :dev, runtime: false},
       {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
-      {:excoveralls, "~> 0.13.0", only: :test, runtime: false},
+      {:excoveralls, "~> 0.14.0", only: :test, runtime: false},
       {:geolix_testdata, "~> 0.5.1", only: [:bench, :test], runtime: false}
     ]
   end
